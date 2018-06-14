2d7da6dc98745d9a82418880b35074dc0fc44ceb
StevenMoon <yisizhu520@163.com>
2018-06-14 09:43:54 +0800

    add fast reduct  roughset 4

---
+++ src/roughset4/Element.java
@@ -0,0 +1,77 @@
+package roughset4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class Element implements Comparable<Element> {
+
+    private List<Integer> valueOfCondition = new ArrayList<Integer>(0);
+    private int valueOfDecision;
+    private int u;
+    private String tempPropertyValue;
+
+    //通过条件属性、决策属性集值与论域中样本的编号初始化样本
+    public Element(List<Integer> condition, int decision, int iNo) {
+        this.valueOfCondition = condition;
+        this.valueOfDecision = decision;
+        this.u = iNo;
+    }
+
+    public List<Integer> getValueOfCondition() {
+        return valueOfCondition;
+    }
+
+    public void setValueOfCondition(List<Integer> valueOfCondition) {
+        this.valueOfCondition = valueOfCondition;
+    }
+
+    public int getValueOfDecision() {
+        return valueOfDecision;
+    }
+
+    public void setValueOfDecision(int valueOfDecision) {
+        this.valueOfDecision = valueOfDecision;
+    }
+
+    public int getU() {
+        return u;
+    }
+
+    public void setU(int u) {
+        this.u = u;
+    }
+
+    public String getTempPropertyValue() {
+        return tempPropertyValue;
+    }
+
+    public void setTempPropertyValue(String tempPropertyValue) {
+        this.tempPropertyValue = tempPropertyValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Element element = (Element) o;
+        boolean conditionSame = true;
+        for (int j = 0; j < valueOfCondition.size(); j++) {
+            if (!valueOfCondition.get(j).equals(element.valueOfCondition.get(j))) {
+                conditionSame = false;
+                break;
+            }
+        }
+        return conditionSame && valueOfDecision == element.valueOfDecision;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(valueOfCondition, valueOfDecision);
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        return this.u - o.u;
+    }
+}
